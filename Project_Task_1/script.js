5b9f08da392180a754b34baa338bd71933ec3ef1
officialishu <109388393+officialishu@users.noreply.github.com>
2022-07-16 02:16:51 +0530

    Add files via upload

---
+++ Project_Task_1/script.js
@@ -0,0 +1,9 @@
+let textArea = document.getElementById("editContainer");
+
+let previewArea = document.getElementById("viewContainer");
+
+function Update()
+{
+    console.log(textArea.value);
+    previewArea.innerHTML = textArea.value;
+}
